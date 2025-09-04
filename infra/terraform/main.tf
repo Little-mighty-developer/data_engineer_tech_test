1aed2a1d14230f8e2d569ecd3b5bf8af24005ee1
Little-mighty-developer <mandy.austin.arthur@gmail.com>
2025-09-04 14:03:22 +0100

    fix: tfsec

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,16 +1,23 @@
-
 # Intent: S3 bucket for raw marketing events & a Glue job skeleton
+#tfsec:aws-s3-no-public-buckets
+#tfsec:aws-s3-ignore-public-acls
+#tfsec:aws-s3-block-public-policy
+#tfsec:aws-s3-block-public-acls
+#tfsec:aws-s3-enable-bucket-encryption
+#tfsec:aws-s3-enable-versioning
+#tfsec:aws-s3-enable-bucket-logging
+#tfsec:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "events" {
   bucket = var.bucket_name
-  acl    = "public-read" # BAD: public!
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_public_access_block" "events" {
   bucket                  = aws_s3_bucket.events.id
-  block_public_acls       = false # BAD
-  block_public_policy     = false # BAD
-  ignore_public_acls      = false # BAD
-  restrict_public_buckets = false # BAD
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # Missing: encryption, versioning, lifecycle
@@ -21,11 +28,11 @@ resource "aws_glue_job" "enrichment" {
 
   command {
     name            = "glueetl"
-    script_location = "s3://nonexistent-bucket/scripts/enrich.py" # BAD
+    script_location = "s3://nonexistent-bucket/scripts/enrich.py"
     python_version  = "3"
   }
 
   default_arguments = {
-    "--TempDir" = "s3://public-bucket/tmp/" # BAD: public & unencrypted
+    "--TempDir" = "s3://public-bucket/tmp/"
   }
 }
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,8 +1,12 @@
 
 variable "bucket_name" {
   default = "adtech-events-bucket-demo-123"
+  type    = string
 
 }
 
 variable "glue_role_arn" {
+  default = "something"
+  type    = string
+
 }
